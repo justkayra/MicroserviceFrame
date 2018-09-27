8453350a7328448accbc3cbbe66df3ba525ca791
kaira <biosubj@gmail.com>
2018-09-27 09:38:12 +0300

    #

--- src/main/java/com/justkaira/ms/MicroserviseFrameServiceApplication.java
+++ src/main/java/com/justkaira/ms/MicroserviseFrameServiceApplication.java
@@ -1,16 +1,14 @@
 package com.justkaira.ms;
 
+import com.justkaira.spring.MsApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-import com.justkaira.spring.MsApplicationRunner;
-
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = { SecurityAutoConfiguration.class })
 public class MicroserviseFrameServiceApplication extends MsApplicationRunner {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviseFrameServiceApplication.class, args);
 	}
